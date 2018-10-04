1a16c9fd4718f93c4614dd9040ed504301eec111
Gerardo <gerardofd1079@gmail.com>
2018-10-04 15:28:14 +0900

    Use webbrowser, make song list and choose function

--- YouTubeAlarm.py
+++ YouTubeAlarm.py
@@ -1,3 +1,9 @@
-import os
+import webbrowser
+import random
 
-os.system("start \"\" https://www.youtube.com/watch?v=UJFYOkQDB20")
+def choose_song():
+    return song_list[random.randint(0,2)]
+
+song_list = ["https://www.youtube.com/watch?v=UJFYOkQDB20", "https://www.youtube.com/watch?v=MkD_kYkRk3c", "https://www.youtube.com/watch?v=HrLjbJtMfVk"]
+
+webbrowser.open(choose_song())
